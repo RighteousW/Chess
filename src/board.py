6f3f73b6ee394c45ddad3962c4fdbfd38a378e02
Righteous Wasambo <rjwasambo@gmail.com>
2024-04-16 19:51:47 +0200

    added en-passant

--- src/board.py
+++ src/board.py
@@ -15,24 +15,24 @@ class Board:
     def __init__(self, screen: surface):
         self.screen: surface = screen
 
-        self.has_focus: bool = False  # if a valid piece has been clicked
-        self.correct_player_focus: bool = False  # if focused piece is of the correct player
-        self.focused_piece: Piece = DEFAULT_PIECE  # initialised as a non-valid piece
+        self.has_selected_piece: bool = False  # if a valid piece has been clicked
+        self.selected_piece: Piece = DEFAULT_PIECE  # initialised as a non-valid piece
 
-        # use every chess piece's location as an index to it in the dictionary
+        # use every chess piece's location as an index to itself in the dictionary
         self.chess_pieces: dict[(int, int):Piece] = dict()
 
-        # piece to test movement
-        # self.chess_pieces[4.0, 4.0] = Piece(Vector2(4.0, 4.0), 'bishop', 'white')
-
-        # determine who plays
+        # determines who plays
         self.player_turn: str = 'white'
+
+        # contains pawns that have recently jumped a tile
+        self.black_pawn_jump: list[tuple] = list()
+        self.white_pawn_jump: list[tuple] = list()
 
         self.initialize_pieces()
 
-        self.grid_change: float = SCREEN_HEIGHT / 10  # distance between tiles in pixels
+        self.tile_change: float = SCREEN_HEIGHT / 10  # distance between tiles in pixels
 
-    # initial locations of chess_pieces
+    # initial positions of chess_pieces
     def initialize_pieces(self):
 
         # initialize pawns
@@ -40,29 +40,29 @@ class Board:
             self.chess_pieces[_, 2] = Piece(Vector2(_, 2), 'pawn', 'black')
             self.chess_pieces[_, 7] = Piece(Vector2(_, 7), 'pawn', 'white')
 
-        # initialize castles
+        # initialize rooks
         self.chess_pieces[1, 1] = Piece(Vector2(1, 1), 'rook', 'black')
         self.chess_pieces[8, 1] = Piece(Vector2(8, 1), 'rook', 'black')
         self.chess_pieces[1, 8] = Piece(Vector2(1, 8), 'rook', 'white')
         self.chess_pieces[8, 8] = Piece(Vector2(8, 8), 'rook', 'white')
 
-        # initialize knight
+        # initialize knights
         self.chess_pieces[2, 1] = Piece(Vector2(2, 1), 'knight', 'black')
         self.chess_pieces[7, 1] = Piece(Vector2(7, 1), 'knight', 'black')
         self.chess_pieces[2, 8] = Piece(Vector2(2, 8), 'knight', 'white')
         self.chess_pieces[7, 8] = Piece(Vector2(7, 8), 'knight', 'white')
 
-        # initialize bishop
+        # initialize bishops
         self.chess_pieces[3, 1] = Piece(Vector2(3, 1), 'bishop', 'black')
         self.chess_pieces[6, 1] = Piece(Vector2(6, 1), 'bishop', 'black')
         self.chess_pieces[3, 8] = Piece(Vector2(3, 8), 'bishop', 'white')
         self.chess_pieces[6, 8] = Piece(Vector2(6, 8), 'bishop', 'white')
 
-        # initialize queen
+        # initialize queens
         self.chess_pieces[4, 1] = Piece(Vector2(4, 1), 'queen', 'black')
         self.chess_pieces[4, 8] = Piece(Vector2(4, 8), 'queen', 'white')
 
-        # initialize king
+        # initialize kings
         self.chess_pieces[5, 1] = Piece(Vector2(5, 1), 'king', 'black')
         self.chess_pieces[5, 8] = Piece(Vector2(5, 8), 'king', 'white')
 
@@ -75,21 +75,20 @@ class Board:
                 else:
                     grid_color = LIGHT_TILE_COLOR
                 pygame.draw.rect(self.screen, grid_color,
-                                 Rect(x * self.grid_change, y * self.grid_change, self.grid_change, self.grid_change))
+                                 Rect(x * self.tile_change, y * self.tile_change, self.tile_change, self.tile_change))
 
-    # draw lines on borders that makes the tiles more distinct
+    # draw lines on tile borders that makes the tiles more distinct
     def draw_lines(self):
-        # horizontal lines
         for _ in range(1, 10):
+            # horizontal lines
             pygame.draw.line(self.screen, COLOR_WHITE,
-                             (self.grid_change, _ * self.grid_change),
-                             (self.grid_change * 9, _ * self.grid_change))
+                             (self.tile_change, _ * self.tile_change),
+                             (self.tile_change * 9, _ * self.tile_change))
 
-        # vertical lines
-        for _ in range(1, 10):
+            # vertical lines
             pygame.draw.line(self.screen, COLOR_WHITE,
-                             (_ * self.grid_change, self.grid_change),
-                             (_ * self.grid_change, self.grid_change * 9))
+                             (_ * self.tile_change, self.tile_change),
+                             (_ * self.tile_change, self.tile_change * 9))
 
     # draw images of chess_pieces
     def draw_chess_pieces(self):
@@ -97,38 +96,30 @@ class Board:
             piece_image_file = CHESS_PIECE_IMAGES[f'{chess_piece.type}_{chess_piece.color}']
             piece_image = resize_image(piece_image_file, 40)
 
-            self.screen.blit(piece_image, (chess_piece.location.x * self.grid_change + 10,
-                                           chess_piece.location.y * self.grid_change + 10))
+            self.screen.blit(piece_image, (chess_piece.location.x * self.tile_change + 10,
+                                           chess_piece.location.y * self.tile_change + 10))
 
     # changes tile color of focused tile to cyan and tiles it can move to as light cyan
     def draw_focused_piece(self):
         # check if selected tile is valid
-        if self.focused_piece != DEFAULT_PIECE:
+        if self.selected_piece != DEFAULT_PIECE:
 
             # make color of selected tile cyan
-            pygame.draw.rect(self.screen, COLOR_CYAN,
-                             Rect(self.focused_piece.location.x * self.grid_change,
-                                  self.focused_piece.location.y * self.grid_change,
-                                  self.grid_change,
-                                  self.grid_change))
+            pygame.draw.rect(self.screen, COLOR_SELECTED_PIECE,
+                             Rect(self.selected_piece.location.x * self.tile_change,
+                                  self.selected_piece.location.y * self.tile_change,
+                                  self.tile_change,
+                                  self.tile_change))
 
             # make color of possible tiles light cyan
-            for (x, y) in self.possible_places(self.focused_piece):
-                if (x, y) in self.chess_pieces.keys():
-                    if self.chess_pieces[x, y].color != self.focused_piece.color:
-                        pygame.draw.rect(self.screen, COLOR_LIGHT_CYAN,
-                                         Rect(x * self.grid_change,
-                                              y * self.grid_change,
-                                              self.grid_change,
-                                              self.grid_change))
-                else:
-                    pygame.draw.rect(self.screen, COLOR_LIGHT_CYAN,
-                                     Rect(x * self.grid_change,
-                                          y * self.grid_change,
-                                          self.grid_change,
-                                          self.grid_change))
+            for (x, y) in self.possible_places(self.selected_piece):
+                pygame.draw.rect(self.screen, COLOR_POSSIBLE_TILES,
+                                 Rect(x * self.tile_change,
+                                      y * self.tile_change,
+                                      self.tile_change,
+                                      self.tile_change))
 
-    # renders the board, the borderlines and chess chess_pieces
+    # renders the board, the borderlines and chess_pieces
     def render(self):
         self.draw_board()
         self.draw_focused_piece()
@@ -144,34 +135,69 @@ class Board:
         x, y = location
 
         # convert clicked pixel coordinates to tile coordinates
-        x = (x - (x % self.grid_change)) / self.grid_change
-        y = (y - (y % self.grid_change)) / self.grid_change
+        x = (x - (x % self.tile_change)) / self.tile_change
+        y = (y - (y % self.tile_change)) / self.tile_change
 
         # if a piece is already selected try to move it to the clicked tile
-        if self.has_focus and self.focused_piece.color == self.player_turn and in_bounds(x, y):
-            if (x, y) in self.possible_places(self.focused_piece):
+        if self.has_selected_piece and self.selected_piece.color == self.player_turn and in_bounds(x, y):
+            if (x, y) in self.possible_places(self.selected_piece):
                 is_empty_space = (x, y) not in self.chess_pieces
 
-                # only move if destination tile is an enemy piece or an empty tile
-                if is_empty_space or (not is_empty_space and self.chess_pieces[x, y].color != self.focused_piece.color):
-                    self.has_focus = False
-                    self.chess_pieces[x, y] = Piece(Vector2(x, y), self.focused_piece.type, self.focused_piece.color)
-                    self.chess_pieces.pop((self.focused_piece.location.x, self.focused_piece.location.y))
-                    self.focused_piece = DEFAULT_PIECE
+                # on event of en-passant
+                if self.can_en_passant_to((x, y)):
+                    self.chess_pieces[(x, y)] = Piece(Vector2((x, y)),
+                                                      self.selected_piece.type, self.selected_piece.color)
+
+                    # remove previous location of selected piece and location of captured pawn
+                    self.chess_pieces.pop((self.selected_piece.location.x, self.selected_piece.location.y))
+                    if self.selected_piece.color == 'white':
+                        self.chess_pieces.pop((x, y + 1))
+                    else:
+                        self.chess_pieces.pop((x, y - 1))
+
+                # only move if destination tile has an enemy piece or is an empty tile
+                elif is_empty_space or (not is_empty_space and
+                                        self.chess_pieces[x, y].color != self.selected_piece.color):
+
+                    self.chess_pieces[x, y] = Piece(Vector2(x, y), self.selected_piece.type, self.selected_piece.color)
+
+                    # remove previous location of selected piece
+                    self.chess_pieces.pop((self.selected_piece.location.x, self.selected_piece.location.y))
+
+                    # keeping location of pawn that moved two spaces on first move, info needed for en passant
+                    if (self.selected_piece.type == 'pawn' and
+                            self.selected_piece.location.y == 2 or
+                            self.selected_piece.location.y == 7):
+
+                        if self.selected_piece.color == 'white':
+                            if y == self.selected_piece.location.y - 2:
+                                self.white_pawn_jump.append((x, y))
+
+                        if self.selected_piece.color == 'black':
+                            if y == self.selected_piece.location.y + 2:
+                                self.black_pawn_jump.append((x, y))
+
+                    # only allow en passant on opponents first move following pawn jump
+                    if self.selected_piece.color == 'white':
+                        self.black_pawn_jump.clear()
+                    if self.selected_piece.color == 'black':
+                        self.white_pawn_jump.clear()
+
+                    self.has_selected_piece = False
+                    self.selected_piece = DEFAULT_PIECE
                     self.chess_pieces[x, y].has_moved = True
                     self.switch_player()
-
                 else:
-                    self.has_focus = True
-                    self.focused_piece = self.chess_pieces[x, y]
+                    self.has_selected_piece = False
+                    self.selected_piece = DEFAULT_PIECE
             else:
-                self.has_focus = True
-                self.focused_piece = self.chess_pieces[x, y]
+                self.has_selected_piece = False
+                self.selected_piece = DEFAULT_PIECE
 
         # if no piece is selected, select piece at clicked tile
         elif (x, y) in self.chess_pieces:
-            self.has_focus = True
-            self.focused_piece = self.chess_pieces[x, y]
+            self.has_selected_piece = True
+            self.selected_piece = self.chess_pieces[x, y]
 
     # returns possible locations the piece can go to at current position
     def possible_places(self, piece: Piece) -> list[tuple]:
@@ -200,28 +226,51 @@ class Board:
         return result
 
     def pawn_capture(self, result, pawn_piece: Piece):
-        right_black = (pawn_piece.location.x - 1, pawn_piece.location.y + 1)
-        right_white = (pawn_piece.location.x + 1, pawn_piece.location.y - 1)
-        left_black = (pawn_piece.location.x + 1, pawn_piece.location.y + 1)
-        left_white = (pawn_piece.location.x - 1, pawn_piece.location.y - 1)
+        top_right_black = (pawn_piece.location.x - 1, pawn_piece.location.y + 1)
+        top_right_white = (pawn_piece.location.x + 1, pawn_piece.location.y - 1)
+        top_left_black = (pawn_piece.location.x + 1, pawn_piece.location.y + 1)
+        top_left_white = (pawn_piece.location.x - 1, pawn_piece.location.y - 1)
 
+        left_white = (pawn_piece.location.x - 1, pawn_piece.location.y)
+        right_white = (pawn_piece.location.x + 1, pawn_piece.location.y)
+        left_black = right_white
+        right_black = left_white
+
+        # possible pieces black can capture
         if pawn_piece.color == 'black':
-            if pawn_piece.location.x > 1 and right_black in self.chess_pieces.keys():
-                if self.chess_pieces[right_black].color != pawn_piece.color:
-                    result.append(right_black)
+            # normal captures
+            if pawn_piece.location.x > 1 and top_right_black in self.chess_pieces.keys():
+                if self.chess_pieces[top_right_black].color != pawn_piece.color:
+                    result.append(top_right_black)
 
-            if pawn_piece.location.x < 8 and left_black in self.chess_pieces.keys():
-                if self.chess_pieces[left_black].color != pawn_piece.color:
-                    result.append(left_black)
+            if pawn_piece.location.x < 8 and top_left_black in self.chess_pieces.keys():
+                if self.chess_pieces[top_left_black].color != pawn_piece.color:
+                    result.append(top_left_black)
 
+            # en passant
+            if left_black in self.white_pawn_jump:
+                result.append(top_left_black)
+
+            elif right_black in self.white_pawn_jump:
+                result.append(top_right_black)
+
+        # possible pieces white can capture
         elif pawn_piece.color == 'white':
-            if pawn_piece.location.x > 1 and left_white in self.chess_pieces.keys():
-                if self.chess_pieces[left_white].color != pawn_piece.color:
-                    result.append(left_white)
+            # normal captures
+            if pawn_piece.location.x > 1 and top_left_white in self.chess_pieces.keys():
+                if self.chess_pieces[top_left_white].color != pawn_piece.color:
+                    result.append(top_left_white)
 
-            if pawn_piece.location.x < 8 and right_white in self.chess_pieces.keys():
-                if self.chess_pieces[right_white].color != pawn_piece.color:
-                    result.append(right_white)
+            if pawn_piece.location.x < 8 and top_right_white in self.chess_pieces.keys():
+                if self.chess_pieces[top_right_white].color != pawn_piece.color:
+                    result.append(top_right_white)
+
+            # en passant
+            if left_white in self.black_pawn_jump:
+                result.append(top_left_white)
+
+            if right_white in self.black_pawn_jump:
+                result.append(top_right_white)
 
         return result
 
@@ -442,3 +491,29 @@ class Board:
                 result.append(bottom)
             elif self.chess_pieces[bottom].color != piece.color:
                 result.append(bottom)
+
+    # returns if en passant is possible
+    def can_en_passant_to(self, location) -> bool:
+        # return false if selected piece is not a pawn or can't go to destination tile
+        if self.selected_piece.type != 'pawn' or location not in self.possible_places(self.selected_piece):
+            return False
+
+        x, y = location
+        if self.selected_piece.color == 'white':
+            # return false if piece below destination tile not in list of all piece locations
+            if (x, y + 1) not in self.chess_pieces.keys():
+                return False
+            # return true if piece below destination tile in list of recently jumped enemy pieces
+            if (x, y + 1) in self.black_pawn_jump:
+                return True
+            return False
+
+        elif self.selected_piece.color == 'black':
+            # return false if piece below destination tile not in list of all piece locations
+            if (x, y - 1) not in self.chess_pieces.keys():
+                return False
+            # return true if piece below destination tile in list of recently jumped enemy pieces
+            if (x, y - 1) in self.white_pawn_jump:
+                return True
+            return False
+        return False
